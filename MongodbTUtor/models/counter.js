274fd309d17b4f132a80d053e5453dc3b608044e
praveenk222 <praveenkadma222@gmail.com>
2024-11-24 20:22:01 +0530

    lookup

---
+++ MongodbTUtor/models/counter.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const counterSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  seq: { type: Number, default: 0 },
+});
+
+const Counter = mongoose.model('Counter', counterSchema);
